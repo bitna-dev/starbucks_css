e50831affdf813cdafb6c94d8be6293de9ce6314
Bitna <lolxx1451@gmail.com>
2023-07-11 15:06:41 -0700

    visual section

--- js/main.js
+++ js/main.js
@@ -13,3 +13,34 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 2000) {
+      // 배지 숨기기
+
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      // 배지 보여주기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
+// _.throttle(함수, 시간);
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.6,
+    opacity: 1,
+  });
+});
